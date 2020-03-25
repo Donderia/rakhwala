505b12e94886450710f421931542385794f9c785
Akash <akash.roy@mobileum.com>
2020-03-25 14:31:40 +0530

    akash : horizontal fix and map scroll trap fix

--- js/custom-rakhwala.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click",function(e){
-    var menu = document.getElementById("overlay");
-    var target = e.target;
-    if(target !== menu && !menu.contains(target)){
-       menu.style.display = "none";
-    }
-});
